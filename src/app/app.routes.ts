9c5388ea38767e3a35a2bef10a7d5dd6f99471af
buhleqampi <buhle3857@gmail.com>
2024-09-25 16:02:42 +0200

    standalone works

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
+import { ParkingListComponent } from './components/parking-list/parking-list.component';
+import { ParkingDetailComponent } from './components/parking-detail/parking-detail.component';
 
 export const routes: Routes = [
 
-    
+    {path:'', redirectTo:'/parking-list', pathMatch: 'full'},
+    {path:'parking-list', component:ParkingListComponent},
+    {path:'parking-detail', component:ParkingDetailComponent}
 ];

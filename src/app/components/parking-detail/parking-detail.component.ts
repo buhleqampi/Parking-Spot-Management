d3f6c54b78e7a71256d047f2247ab6e17656cc2e
buhleqampi <buhle3857@gmail.com>
2024-09-25 14:51:12 +0200

    first commit

---
+++ src/app/components/parking-detail/parking-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-parking-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './parking-detail.component.html',
+  styleUrl: './parking-detail.component.css'
+})
+export class ParkingDetailComponent {
+
+}
